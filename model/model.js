ae2093fe6fbafb0a7e68922b1899b0e8b99d4f55
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-16 15:52:19 +0430

    model's first initialize

---
+++ model/model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fName: {
+        type: String,
+        required: true,
+        maxlength: 20,
+        trim: true
+    },
+    lName: {
+        type: String,
+        required: true,
+        maxlength: 20,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    contacts: [{
+        contact: {
+            fName: {
+                type: String
+            },
+            lName: {
+                type: String
+            },
+            phone: {
+                type: String
+            }
+        },
+        required: false
+    }],
+    email: {
+        type: String,
+        required: true,
+    }
+});
