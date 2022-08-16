b7127df40bf903b2746b9c347091d531401ea72a
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-16 15:29:17 +0430

    first template

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+const port = 7000;
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+const start = () => {
+    app.listen(port, () => {
+        console.log(`server runnign on port: %{port}`);
+    })
+};
+
+
+start();
